dcc4850301a70389b382c5410f773f4d6636a0ab
Степан Мартынов <smartd88@gmail.com>
2022-04-05 16:16:17 +0300

    added test model entity

--- models/models.py
+++ models/models.py
@@ -1,5 +1,33 @@
 from pydantic import BaseModel
+from tortoise import Tortoise, fields, run_async
+from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
+from tortoise.models import Model
 
+
+class Entity(Model):
+    name = fields.CharField(max_length=255)
+    full_name = fields.TextField(null=True)
+    legal_adress = fields.TextField(null=True)
+    postal_adress = fields.TextField(null=True)
+    tax_id = fields.CharField(max_length=15)
+    kpp = fields.CharField(max_length=15, null=True)
+    okpo = fields.CharField(max_length=15)
+
+    # tax_id = fields.CharField(max_length=15)
+
+    def __repr__(self):
+        return f'{self.name} {self.full_name}'
+
+    def __str__(self):
+        return f'{self.name} {self.full_name}'
+
+
+class Photosession(Model):
+    name = fields.CharField(max_length=255)
+    num = fields.IntField()
+
+
+'''
 class Deal(BaseModel):
     url: str
     service: str
@@ -23,10 +51,28 @@ class Deal(BaseModel):
             'photos': '{{photos}}',
             'num_of_photos': str(len(image_list))
         }
+'''
 
 
+async def run():
+    await Tortoise.init(db_url="sqlite://:memory", modules={"models": ["__main__"]})
+    await Tortoise.generate_schemas()
+    Entity_pydantic = pydantic_model_creator(Entity)
+
+    entity = await Entity.create(
+        name="Puzzle",
+        full_name="АО Пазл",
+        legal_adres="Энгельса 4",
+        postal_adres="Энгельса 114",
+        tax_id="408907345",
+        kpp="8275913749",
+        okpo="8275913749"
+    )
+    print(await Entity.all())
 
 
+if __name__ == '__main__':
+    run_async(run())
 
     # link: str #URL
     # num: str  #contract_number
