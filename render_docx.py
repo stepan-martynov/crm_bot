6b2567e697e375adffa4924614633cd7722c521d
Степан Мартынов <smartd88@gmail.com>
2022-04-18 08:57:27 +0300

    added Entity class

--- render_docx.py
+++ render_docx.py
@@ -1,16 +1,18 @@
 import os
 from datetime import datetime
+
+import pydantic
 from PIL import Image
 from loguru import logger
 import random
-
+import pydantic
 from docx import Document
 from docxtpl import DocxTemplate
 from docx.shared import Cm
 from babel.dates import format_date
 from num2words import num2words
 
-from yadisk_api import get_public_meta, create_img_list, download_preview
+# from yadiskapi import get_public_meta, create_img_list, download_preview
 
 
 # location = ""
@@ -30,13 +32,30 @@ price = 2000
 
 # ========== END INPUT ==========
 
+class Entity(pydantic.BaseModel):
+
+    url: str
+    broker: str
+    customer: str
+    service: str
+    price: int
+
+    def __str__(self):
+        return f'{self.__class__} {self.service} {self.url}'
+
+
+
+
+
+
+
 doc_template = doc_template or "E&V3.docx"
 DOCX_TEMPLATE_PATH = os.path.join(os.getcwd(), doc_template)
 
-dir = get_public_meta(URL)
+# dir = get_public_meta(URL)
 
 litter_price = num2words(price, lang="ru")
-contract_number = f'ev-sm-{dir.name.split(" ")[0]}'  # TODO заменить на порядковый номер контракта из ДБ
+# contract_number = f'ev-sm-{dir.name.split(" ")[0]}'  # TODO заменить на порядковый номер контракта из ДБ
 
 
 def filter_img_list(image_list: list, img_filter: str) -> list:
@@ -55,31 +74,31 @@ def date_format_to_ru(d: datetime) -> str:
     return format_date(d, format='long', locale='ru')[:-3]
 
 
-image_list = create_img_list(URL)
-image_list = filter_img_list(image_list, img_filter) if img_filter else image_list
+# image_list = create_img_list(URL)
+# image_list = filter_img_list(image_list, img_filter) if img_filter else image_list
 
-shooting_date = random.choice(image_list).name[:8]
-contract_date = date_format_to_ru(datetime.strptime(shooting_date, "%Y%m%d"))  # '9 сентября 2020'
-short_date = datetime.strptime(shooting_date, "%Y%m%d").strftime("%d.%m.%Y")
-cur_date = date_format_to_ru(datetime.now())
+# shooting_date = random.choice(image_list).name[:8]
+# contract_date = date_format_to_ru(datetime.strptime(shooting_date, "%Y%m%d"))  # '9 сентября 2020'
+# short_date = datetime.strptime(shooting_date, "%Y%m%d").strftime("%d.%m.%Y")
+# cur_date = date_format_to_ru(datetime.now())
 
-location = location or 'Санкт-Петербург'
-service_name = service_name or "Профессиональная фотосъемка недвижимости"
-context = {
-    'link': URL,
-    'num': contract_number,
-    'NUM': contract_number.upper(),
-    'date': contract_date,
-    'DATE': contract_date.upper(),
-    'cost': str(price),
-    'letter_cost': litter_price,
-    'service_name': service_name,
-    'final_cost': str(price),
-    'cur_date': cur_date,
-    'adress': f'{location}, {dir.name[4:]}',
-    'photos': '{{photos}}',
-    'num_of_photos': str(len(image_list))
-}
+# location = location or 'Санкт-Петербург'
+# service_name = service_name or "Профессиональная фотосъемка недвижимости"
+# context = {
+#     'link': URL,
+#     'num': contract_number,
+#     'NUM': contract_number.upper(),
+#     'date': contract_date,
+#     'DATE': contract_date.upper(),
+#     'cost': str(price),
+#     'letter_cost': litter_price,
+#     'service_name': service_name,
+#     'final_cost': str(price),
+#     'cur_date': cur_date,
+#     'adress': f'{location}, {dir.name[4:]}',
+#     'photos': '{{photos}}',
+#     'num_of_photos': str(len(image_list))
+# }
 
 
 def add_images(image_list, doc):
@@ -104,7 +123,7 @@ def add_images(image_list, doc):
 
 
 if __name__ == '__main__':
-    doc = DocxTemplate(DOCX_TEMPLATE_PATH)
+    # doc = DocxTemplate(DOCX_TEMPLATE_PATH)
     # doc.render(context)
     # file_name = f"{contract_number}.docx"
     # doc.save(file_name)
@@ -122,10 +141,22 @@ if __name__ == '__main__':
     # print(str_to_exel)
 
     # doc = Document(file_name)
-    doc = Document(doc_template)
-    add_images(image_list, doc)
+    # doc = Document(doc_template)
+    # add_images(image_list, doc)
     # doc.save(file_name)
-    doc.save(doc_template)
+    # doc.save(doc_template)
     # print(upload_file(URL, file_name))
 
     # os.remove(file_name)
+
+    data = {
+        "url": "https://",
+        "broker": "Анна Романова",
+        "customer": "e&v",
+        "service": "Город",
+        "price": 3000
+    }
+
+    ent1 = Entity.parse_obj(data)
+
+    print(ent1)
